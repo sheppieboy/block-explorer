8d02161bce31ab9f3f39e9470fffcdc39cc7d61f
Luke Sheppard <sheppiegit@gmail.com>
2023-06-21 16:18:43 -0400

    initial server.

--- server/index.js
+++ server/index.js
@@ -1,22 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const { Alchemy, Network } = require('alchemy-sdk');
 
 dotenv.config();
 const server = express();
+server.use(express.json());
 
-mongoose.set('strictQuery', false);
-
-const start_db_server = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_CONNECTION);
-    console.log('MongoDB connected');
-    server.listen(process.env.PORT_NUM, () => {
-      console.log(`Server is up and running at port ${process.env.PORT_NUM}`);
-    });
-  } catch (err) {
-    console.error('Error connecting to MongoDB', err);
-  }
-};
-
-start_db_server();
+server.listen(process.env.PORT_NUM, () => {
+  console.log(`Server listening on port: ${process.env.PORT_NUM}`);
+});
