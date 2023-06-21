4da28c6eed86db1e7b3e05c446b1313b8228bf6f
Luke Sheppard <sheppiegit@gmail.com>
2023-06-21 17:16:04 -0400

    Added a function to get the current block.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const ethers = require('ethers');
 const { PrismaClient } = require('@prisma/client');
+const { Alchemy, Network } = require('alchemy-sdk');
+
+const settings = {
+  apiKey: process.env.ALCHEMY_API_KEY,
+  network: Network.ETH_MAINNET,
+};
+
+const alchemy = new Alchemy(settings);
 
 const prisma = new PrismaClient();
 
@@ -17,3 +26,54 @@ server.listen(process.env.PORT_NUM, () => {
       console.log('Error connecting to database', err);
     });
 });
+
+const getBlockInfo = async () => {
+  const blockNumber = await alchemy.core.getBlockNumber();
+
+  //get block
+  let block = await alchemy.core.getBlock(blockNumber);
+
+  //set BigNumbers to strings
+  block.gasLimit = block.gasLimit.toString();
+  block.gasUsed = block.gasUsed.toString();
+};
+
+const writeToDB = async (block) => {
+  const {
+    hash,
+    parentHash,
+    number,
+    timestamp,
+    nonce,
+    difficulty,
+    gasLimit,
+    gasUsed,
+    miner,
+    transactions,
+  } = block;
+
+  await prisma.block.create({
+    data: {
+      hash: hash,
+      parentHash: parentHash,
+      blockNumber: number,
+      nonce: nonce,
+      difficulty: difficulty,
+      gasLimit: gasLimit,
+      gasUsed: gasUsed,
+      miner: miner,
+      transactionCount: transactions.length,
+    },
+  });
+};
+
+const readNewBlock = async (hash) => {
+  const block = await prisma.block.findUnique({
+    where: {
+      hash: hash,
+    },
+  });
+  console.log(block);
+};
+
+getBlockInfo();
